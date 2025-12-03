fb6642b83ce932d8686ce227765f6c67a4982518
dmitrylito <dmitrylitoshik15@gmail.com>
2025-12-03 02:33:06 -0500

    Dot files updated

--- dotfiles/.config/nvim/vscode-init.lua
+++ dotfiles/.config/nvim/vscode-init.lua
@@ -20,4 +20,7 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+vim.keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz")
+vim.keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz")
+
 vim.scrolloff = 999
